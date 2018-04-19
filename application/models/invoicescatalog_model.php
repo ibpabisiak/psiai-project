f7ee87ad894bdcb78a66f431e394bc35423fea2a
hochental <35307610+hochental@users.noreply.github.com>
2018-04-19 21:32:01 +0200

    Update invoicescatalog_model.php

--- application/models/invoicescatalog_model.php
+++ application/models/invoicescatalog_model.php
@@ -1,11 +1,27 @@
 <?php
-
-class InvoicesCatalogModel { 
-	
-	private $db;
-
-	function __construct($db) {
-		$this->db = $db;
-	}
-
+ 
+class InvoicesCatalogModel {
+   
+    private $db;
+    private $files;
+ 
+    function __construct($db) {
+        $this->db = $db;
+        $this->files=new Files($db)
+    }
+ 
+    public function listAll() {
+        $query = "SELECT * FROM 'purchase_invoices'";
+        $dbh = $this->db->prepare($query);
+        $dbh->execute();
+ 
+        while ($row = $dbh->fetch(PDO::FETCH_ASSOC)) {
+            $tableList[] = $row[0];
+        }
+        print_r($tableList);
+ 
+ 
+ 
+    }
+ 
 }
