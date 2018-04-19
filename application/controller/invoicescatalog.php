5424072a0e798cc093a2c2367672e10371752b27
hochental <35307610+hochental@users.noreply.github.com>
2018-04-19 21:32:39 +0200

    Update invoicescatalog.php

--- application/controller/invoicescatalog.php
+++ application/controller/invoicescatalog.php
@@ -1,22 +1,25 @@
 <?php
-
+ 
 class InvoicesCatalog extends Controller {
-
-	public function index() {
-        
-		require 'application/views/_common/header.tpl.php';
+ 
+    public function index() {
+       
+        require 'application/views/_common/header.tpl.php';
         require 'application/views/invoicescatalog/invoicescatalog_index.tpl.php';
-        require 'application/views/_common/footer.tpl.php';		
-	}
-	
-	public function first_page() {
-		echo "hehe";
-	}
-	
-	public function second_page() {
-		echo "hehe";
-	}
-	
+        require 'application/views/_common/footer.tpl.php';    
+    }
+   
+    public function list_stuff() {
+        if(Functions::GetUserSession()->IsEntitledToWrite('invoicescatalog')) {
+            $invoices_model = $this->loadModuleModel('invoicescatalog_model');
+            $invoices_model->listAll();        
+        } else {
+            exit("Nie masz do tego uprawnień");
+        }
+    }
+   
+    public function second_page() {
+        echo "hehe";
+    }
+   
 }
-
-
