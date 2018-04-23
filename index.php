01613cb87933b0775787edf6702673b5c7dbeee7
edyta68 <edyta68@gmail.com>
2018-04-23 10:44:37 +0200

    Dodano twitter bootstrap, nowy widok logowania

--- index.php
+++ index.php
@@ -2,11 +2,5 @@
 //load application modules
 require_once 'application/core/autoload.php';
 
-/*
-if(null == $_SESSION[USER_SESSION]) 
-	echo "SESSION IS NULL <br /> <br /> <br />";
-else
-	echo "SESSION IS NOT NULL <br /> <br /> <br />";
-*/
 //start application
 $app = new Application();
