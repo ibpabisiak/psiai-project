ca1b518eab9e2e8d45daebc807f563e556b6657b
hochental <35307610+hochental@users.noreply.github.com>
2018-04-26 18:50:23 +0200

    listowanie licencji

--- application/views/licenses/licenses_index.tpl.php
+++ application/views/licenses/licenses_index.tpl.php
@@ -1,3 +1,30 @@
-<div style="height: 400px; width: 100%;">
-	<h1>licenses index page</h1>
-</div>
+<main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-4">
+    <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+
+
+    </div>
+
+
+    <h2>Katalog licencji</h2>
+    <div class="table-responsive">
+        <table class="table table-striped table-sm">
+            <thead>
+            <tr>
+                <th>Numer inwentarza</th>
+                <th>Klucz seryjny</th>
+                <th>Opis</th>
+                <th>Id faktury</th>
+                <th>Wsparcie techniczne</th>
+                <th>Licencja wazna do</th>
+                <th>Pole notatek</th>
+                <th>Gdzie jest licencja</th>
+                <th></th>
+                <th></th>
+            </tr>
+            </thead>
+            <tbody>
+            <?php echo $invoices_list; ?>
+            </tbody>
+        </table>
+    </div>
+</main>
