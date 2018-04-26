bb67ec79bf3ab7b4e38c0d48683d8e7b8804691d
marcinmig <38530099+marcinmig@users.noreply.github.com>
2018-04-27 00:13:07 +0200

    Update invoicescatalog_index.tpl.php

--- application/views/invoicescatalog/invoicescatalog_index.tpl.php
+++ application/views/invoicescatalog/invoicescatalog_index.tpl.php
@@ -31,19 +31,6 @@
 	      
 	      
 	      
-	      <h1>Wyszukiwanie faktur</h1>
-    <form action="wyniki.php" method="post">
-      Wyszukaj według:
-      <select name="metoda">
-       <option value="invoice_id" />invoice_id
-       <option value="contractor_id" />contractor_id
-       <option value="file_id" />file_id
-      </select>
-      <br /><br />
-      Szukane wyrażenie:
-      <input type="text" name="wyrazenie" />
-      <input type="submit" name="wyszukaj" />
-    </form>
 	      
 	      
 	      
