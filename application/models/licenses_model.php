e0bb9ef95c1981bb392d1f4ab0fa900f38edc124
hochental <35307610+hochental@users.noreply.github.com>
2018-04-26 18:47:19 +0200

    Update licenses_model.php
    model licencji

--- application/models/licenses_model.php
+++ application/models/licenses_model.php
@@ -2,12 +2,44 @@
 
 class LicensesModel { 
 	
-	private $db;
+	 private $db;
+    private $files;
 
-	function __construct($db) {
-		$this->db = $db;
-	}
+    function __construct($db)
+    {
+        $this->db = $db;
+        $this->files = new Files($db);
+    }
 
+    public function listAll()
+    {
+        $query = "
+				SELECT * FROM licenses 
+							
+		";
+        $dbh = $this->db->prepare($query);
+        $dbh->execute();
+
+        $result = "";
+        while ($row = $dbh->fetch(PDO::FETCH_ASSOC)) {
+
+            $result .= "
+                <tr>
+                  <td>" . $row['inventary_number'] . "</td>
+                  <td>" . $row['serial_number'] . "</td>
+                  <td>" . $row['description'] . "</td>
+                  <td>" . $row['id_invoice'] . "</td>
+                  <td>" . $row['technical_support'] . "</td>
+                  <td>" . $row['licenses_till'] . "</td>
+                  <td>" . $row['another_note'] . "</td>
+                  <td>" . $row['whos_is_licenses'] . "</td>
+                </tr>			
+			";
+
+        }
+
+        return $result;
+    }
 }
 
 
