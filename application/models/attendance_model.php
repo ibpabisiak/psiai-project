f7c6b4bfaaefdfdf6b48cceedcf2e2370ed0dc99
hochental <35307610+hochental@users.noreply.github.com>
2018-04-26 18:46:04 +0200

    Update attendance_model.php
    model obecnosci

--- application/models/attendance_model.php
+++ application/models/attendance_model.php
@@ -2,10 +2,39 @@
 
 class AttendanceModel { 
 	
-	private $db;
+	 private $db;
+    private $files;
 
-	function __construct($db) {
-		$this->db = $db;
-	}
+    function __construct($db)
+    {
+        $this->db = $db;
+        $this->files = new Files($db);
+    }
+    public function listAll()
+    {
+        $query = "
+				SELECT * FROM attendance_list 
+							
+		";
+        $dbh = $this->db->prepare($query);
+        $dbh->execute();
+
+        $result = "";
+        while ($row = $dbh->fetch(PDO::FETCH_ASSOC)) {
+
+            $result .= "
+                <tr>
+                  <td>" . $row['date'] . "</td>
+                  <td>" . $row['entry_hour'] . "</td>
+                  <td>" . $row['leave_hour'] . "</td>
+                  <td>".  $row[''].  "</td>
+                  <td>" . $row['addons'] . "</td>
+                </tr>			
+			";
+
+        }
+
+        return $result;
+    }
 
 }
