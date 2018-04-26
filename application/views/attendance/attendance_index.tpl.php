878f1ea97167dd687797f3108a4730d53dc17f72
hochental <35307610+hochental@users.noreply.github.com>
2018-04-26 18:48:48 +0200

    listowanie dzienniczka
    tymczasowa baza utworzona na potrzeby listowania

--- application/views/attendance/attendance_index.tpl.php
+++ application/views/attendance/attendance_index.tpl.php
@@ -1,4 +1,27 @@
-        <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-4">
+<main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-4">
+    <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
 
-			<h1>System obecności</h1>
-		</main>
+
+    </div>
+
+
+    <h2>Dzienniczek</h2>
+    <div class="table-responsive">
+        <table class="table table-striped table-sm">
+            <thead>
+            <tr>
+                <th>Data</th>
+                <th>Godzina przyjscia</th>
+                <th>Godzina wyjscia</th>
+                <th>Ilosc godzin</th>
+                <th>Notka</th>
+                <th></th>
+                <th></th>
+            </tr>
+            </thead>
+            <tbody>
+            <?php echo $invoices_list; ?>
+            </tbody>
+        </table>
+    </div>
+</main>
