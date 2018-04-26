c38287b511be78f4d9361c7b3f5057ba784a6528
hochental <35307610+hochental@users.noreply.github.com>
2018-04-26 18:46:45 +0200

    Update equipment_model.php
    model sprzetu

--- application/models/equipment_model.php
+++ application/models/equipment_model.php
@@ -2,10 +2,43 @@
 
 class EquipmentModel { 
 	
-	private $db;
+	 private $db;
+    private $files;
 
-	function __construct($db) {
-		$this->db = $db;
-	}
+    function __construct($db)
+    {
+        $this->db = $db;
+        $this->files = new Files($db);
+    }
 
+    public function listAll()
+    {
+        $query = "
+				SELECT * FROM equipment 
+							
+		";
+        $dbh = $this->db->prepare($query);
+        $dbh->execute();
+
+        $result = "";
+        while ($row = $dbh->fetch(PDO::FETCH_ASSOC)) {
+
+            $result .= "
+                <tr>
+                  <td>" . $row['inventary_number'] . "</td>
+                  <td>" . $row['description'] . "</td>
+                  <td>" . $row['serial_number'] . "</td>
+                  <td>" . $row['date_of_purschure'] . "</td>
+                  <td>" . $row['invoice_id'] . "</td>
+                  <td>" . $row['warranty'] . "</td>
+                  <td>" . $row['another_note'] . "</td>
+                  <td>" . $row['whos_is_equipment'] . "</td>
+                  <td>" . $row['netto_value'] . "zl</td>
+                </tr>			
+			";
+
+        }
+
+        return $result;
+    }
 }
