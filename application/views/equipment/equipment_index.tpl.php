4f43b10a5cedf1147854f5b3ded27281ebe0e730
hochental <35307610+hochental@users.noreply.github.com>
2018-04-26 18:49:36 +0200

    listowanie sprzetu

--- application/views/equipment/equipment_index.tpl.php
+++ application/views/equipment/equipment_index.tpl.php
@@ -1,3 +1,31 @@
-<div style="height: 400px; width: 100%;">
-	<h1>equipment index page</h1>
-</div>
+<main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-4">
+    <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+
+
+    </div>
+
+
+    <h2>Katalog sprzetu</h2>
+    <div class="table-responsive">
+        <table class="table table-striped table-sm">
+            <thead>
+            <tr>
+                <th>Numer inwenatrzowy</th>
+                <th>Pelna nazwa/opis</th>
+                <th>numer seryjny</th>
+                <th>Data zakupu</th>
+                <th>Numer faktury</th>
+                <th>Gwarancja do</th>
+                <th>Wartosc netto</th>
+                <th>Na czyim stanie jest sprzet</th>
+                <th>Pole notatek</th>
+                <th></th>
+                <th></th>
+            </tr>
+            </thead>
+            <tbody>
+            <?php echo $invoices_list; ?>
+            </tbody>
+        </table>
+    </div>
+</main>
