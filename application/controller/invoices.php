77d69fddd4e8f943aee4f025a3d13ddb29190d20
Kinga Ubańska <kurbanskaa@o2.pl>
2018-05-16 23:30:20 +0200

    invoice validation

--- application/controller/invoices.php
+++ application/controller/invoices.php
@@ -3,39 +3,124 @@
 class Invoices extends Controller {
 
 	private $invoice_type = 'sales'; //TODO jakies lepsze rozwiazanie do tego albo rozbic to na 2 moduly;
-	
+
 	public function index() {
-        
+
 		$invoices_model = $this->loadModuleModel('invoices_model');
 		$contractors_list = $invoices_model->LoadContractorsList();
 		$taxes_list = $invoices_model->LoadTaxesList();
-		
+
 		require 'application/views/_common/header.tpl.php';
         require 'application/views/invoices/invoices_index.tpl.php';
-        require 'application/views/_common/footer.tpl.php';		
+        require 'application/views/_common/footer.tpl.php';
 	}
-	
+
 	public function add_invoice() {
 		if(Functions::GetUserSession()->IsEntitledToWrite('invoices')) {
-			$invoices_model = $this->loadModuleModel('invoices_model');
-			if(isset($_POST['invoice_name']) && isset($_POST['amount_netto']) && isset($_POST['amount_brutto'])) {
+            $invoices_model = $this->loadModuleModel('invoices_model');
+            //if(isset($_POST['invoice_name']) && isset($_POST['amount_netto']) && isset($_POST['amount_brutto'])) {
+
+            $a_file = 0;
+            $a_type = 0;
+            $a_name = 0;
+            $a_netto = 0;
+            $a_brutto = 0;
+            $a_tax = 0;
+            $a_number = 0;
+
+
+
+            if (empty($_POST['fileToUpload'])) {
+                $aa_file = 1;
+            } else {
+                $aa_file = 0;
+            }
+
+            if (empty($_POST['invoice_type'])) {
+                $aa_type = 1;
+            } else {
+                $aa_type = 0;
+            }
+
+            if (empty($_POST['contractor_id'])) {
+                $aa_contractor = 1;
+            } else {
+                $aa_contractor = 0;
+            }
+
+
+            if (empty($_POST['invoice_name'])) {
+                $name = $_POST['invoice_name'];
+                $a_name = preg_match('/^([a-z|A-Z|0-9]{3,20))&/', $name);
+                $aa_name = 1;
+            } else {
+                $aa_name = 0;
+            }
+
+
+            if(empty($_POST['invoice_number'])) {
+                $number = $_POST['invoice_number'];
+
+                if (preg_match('/^([0-9]{1,15})&/', $number)) {
+                    $aa_number = 0;
+                } else {
+                    $aa_number = 1;
+                }
+            } else{
+                $aa_number = 1;
+            }
+
+            if(empty($_POST['amount_netto'])) {
+                $netto = $_POST['amount_netto'];
+                if (preg_match('/^([0-9]{0,5}\\,([0-9]{0-2}))&/', $netto)) {
+                    $aa_netto = 0;
+                } else {
+                    $aa_netto = 1;
+                }
+            }else{ $aa_netto = 1;
+            }
+
+            if(empty($_POST['amount_brutto'])) {
+                $brutto = $_POST['amount_brutto'];
+                if (preg_match('/^([0-9]{0,5}\\,([0-9]{0-2}))&/', $brutto)) {
+                    $aa_brutto = 0;
+                } else {
+                    $aa_brutto = 1;
+                }
+            }else{
+                $aa_brutto = 1;
+            }
+
+
+            if(empty($_POST['tax_id'])) {
+                $aa_tax = 0;
+            } else { $aa_tax = 1; }
+
+
+            if (  ($aa_type == 0) && ($aa_name ==0) && ($aa_netto==0) && ($aa_brutto==0) && ($aa_tax==0) && ($aa_number ==0) && ($aa_contractor ==0)){
 				$invoices_model->AddInvoice(
-					$_FILES["fileToUpload"], 
-					$_POST['contractor_id'], 
-					$_POST['invoice_type'], 
-					$_POST['invoice_name'], 
-					$_POST['amount_netto'], 
+					$_FILES["fileToUpload"],
+					$_POST['contractor_id'],
+					$_POST['invoice_type'],
+					$_POST['invoice_name'],
+					$_POST['amount_netto'],
 					$_POST['amount_brutto'],
 					$_POST['tax_id'],
 					$_POST['invoice_number']
 				);
+			} else {
+                require 'application/views/_common/header.tpl.php';
+                require 'application/views/invoices/invoices_validation.tpl.php';
+                require 'application/views/_common/footer.tpl.php';
+            }
 
-			}			
+
 		} else {
 			exit("Nie masz do tego uprawnień");
 		}
 
 	}
+
 
 	public function add_new_contractor() {
 		if(Functions::GetUserSession()->IsEntitledToWrite('invoices')) {
---
+++ application/views/invoices/invoices_validation.tpl.php
@@ -0,0 +1,156 @@
+
+<main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-4" style="padding-top: 20px;">
+    <div id="accordion" style="margin-top: 50px;">
+        <div class="card">
+            <div class="card-header" id="headingOne">
+                <h5 class="mb-0">
+                    <button class="btn btn-link" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                        Dodaj nowego kontrahenta
+                    </button>
+                </h5>
+            </div>
+
+            <div id="collapseOne" class="collapse" aria-labelledby="headingOne" data-parent="#accordion">
+                <div class="card-body" style="width: 50%; margin: auto auto auto auto;">
+                    <form method="POST" action="index.php?module=invoices&page=add_new_contractor">
+                        <div class="form-group">
+                            <label for="formGroupExampleInput">Skrócona nazwa kontrahenta (wymagane):</label>
+                            <input name="contractor_name" type="text" class="form-control" id="formGroupExampleInput" placeholder="Nazwa">
+                            <?php
+                            if ($aa_name==1) {
+                                echo "Brak nazwy kontrahenta";
+                            }
+                            ?>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="formGroupExampleInput2">NIP kontrahenta (opcjonalnie):</label>
+                            <input name="contracotr_nip" type="text" class="form-control" id="formGroupExampleInput2" placeholder="NIP">
+                            <?php
+                            if ($aa_nip==1) {
+                                echo "Podano błędny NIP";
+                            }
+                            ?>
+                        </div>
+                        <div class="form-group">
+                            <button type="submit" class="btn btn-primary btn-lg btn-block">Zapisz w bazie danych</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+        <div class="card">
+            <div class="card-header" id="headingTwo">
+                <h5 class="mb-0">
+                    <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                        Dodaj nową fakturę
+                    </button>
+                </h5>
+            </div>
+            <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordion">
+                <div class="card-body" style="width: 50%; margin: auto auto auto auto;">
+
+                    <form method="POST" action="index.php?module=invoices&page=add_invoice" enctype="multipart/form-data">
+
+                        <div class="form-group">
+                            <label for="formGroupExampleInput2">Skan faktury na twoim dysku (tylko .pdf):</label>
+                            <div class="custom-file">
+                                <input type="file" name="fileToUpload" class="custom-file-input" id="inputGroupFile01">
+                                <label class="custom-file-label" for="inputGroupFile01">Wybierz plik..</label>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label for="formGroupExampleInput2">Kontrahent:</label>
+                            <select name="contractor_id" id="inputState" class="form-control">
+                                <option value="0" selected>Wybierz..</option>
+                                <?php echo $contractors_list; ?>
+                                <?php
+                                if ($aa_contractor==1) {
+                                    echo "Nie wybrano kontraheta";
+                                }
+                                ?>
+                            </select>
+
+                        </div>
+                        <div class="form-group">
+                            <label for="formGroupExampleInput2">Typ faktury:</label>
+                            <select name="invoice_type" id="inputState" class="form-control">
+                                <option value="0" selected>Wybierz..</option>
+
+                                <option value="purchase">Sprzedaży</option>
+                                <option value="sales">Zakupu</option>
+                            </select>
+                            <?php
+                            if ($aa_type==1) {
+                                echo "Nie wybrano typu faktury";
+                            }
+                            ?>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="formGroupExampleInput2">Nazwa faktury:</label>
+                            <input name="invoice_name" type="text" class="form-control" id="formGroupExampleInput2" placeholder="Wprowadź nazwę faktury..">
+                            <?php
+                            if ($aa_name==1) {
+                                echo "Brak nazwy faktury";
+                            }
+                            ?>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="formGroupExampleInput2">Numer faktury:</label>
+                            <input name="invoice_number" type="text" class="form-control" id="formGroupExampleInput2" placeholder="Wprowadź numer faktury..">
+
+                            <?php
+                            if ($aa_number==1) {
+                                echo "Brak numeru faktury";
+                            }
+                            ?>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="formGroupExampleInput2">Kwota netto:</label>
+                            <input name="amount_netto" type="text" class="form-control" id="formGroupExampleInput2" placeholder="Wprowadź kwotę netto..">
+
+                            <?php
+                            if ($aa_netto==1) {
+                                echo "Błędna kwota netto";
+                            }
+                            ?>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="formGroupExampleInput2">Kwota brutto:</label>
+                            <input name="amount_brutto" type="text" class="form-control" id="formGroupExampleInput2" placeholder="Wprowadź kwotę brutto..">
+
+                            <?php
+                            if ($aa_brutto==1) {
+                                echo "Błędna kwota brutto";
+                            }
+                            ?>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="formGroupExampleInput2">Podatek:</label>
+                            <select name="tax_id" id="inputState" class="form-control">
+                                <option value="0" selected>Wybierz..</option>
+
+                                <?php echo $taxes_?>
+                                <?php
+                                if ($aa_tax==1) {
+                                    echo "Nie wybrano podatku";
+                                }
+                                ?>
+                        </div>
+                        <div class="form-group">
+                            <button type="submit" class="btn btn-primary btn-lg btn-block">Zapisz w bazie danych</button>
+                        </div>
+                    </form>
+
+
+                </div>
+            </div>
+        </div>
+    </div>
+
+</main>
