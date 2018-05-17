405601de698d98991057852906c6309bb2b979c6
Kinga Ubańska <kurbanskaa@o2.pl>
2018-05-17 23:46:27 +0200

    documents validation

--- application/controller/documents.php
+++ application/controller/documents.php
@@ -74,16 +74,35 @@ class Documents extends Controller {
 	
 	public function add_document() {
 		if(Functions::GetUserSession()->IsEntitledToWrite('documents')) {
-			$documents_model = $this->loadModuleModel('documents_model');		
+			$documents_model = $this->loadModuleModel('documents_model');
 
-			$documents_model->AddDocument(
-				$_FILES["fileToUpload"],
-				$_POST['document_id'],
-				$_POST['document_notes']
-			);
+            $file = 0;
+			$id = 	0;
+
+            if (empty($_POST['document_id'])) {
+                $id = 1;
+            } else {
+                $id= 0;
+            }
+
+            if( ($file == 0) && ($id == 0)) {
+                $documents_model->AddDocument(
+                    $_FILES["fileToUpload"],
+                    $_POST['document_id'],
+                    $_POST['document_notes']);
 
 
-			header("Location: index.php?module=documents&page=documents_catalog");			
+                header("Location: index.php?module=documents&page=documents_catalog");
+
+			}else{
+                require 'application/views/_common/header.tpl.php';
+                require 'application/views/documents/documents_validation.tpl.php';
+                require 'application/views/_common/footer.tpl.php';
+
+            }
+
+
+
 		}
 	}
 	
---
+++ application/views/documents/documents_validation.tpl.php
@@ -0,0 +1,56 @@
+
+<main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-4" style="padding-top: 20px;">
+    <div id="accordion" style="margin-top: 50px;">
+
+        <div class="card">
+            <div class="card-header" id="headingTwo">
+                <h5 class="mb-0">
+                    Dodaj nowy dokument
+                </h5>
+            </div>
+            <div id="collapseTwo" class="collapse show" aria-labelledby="headingTwo" data-parent="#accordion">
+                <div class="card-body" style="width: 50%; margin: auto auto auto auto;">
+
+                    <form method="POST" action="index.php?module=documents&page=add_document" enctype="multipart/form-data">
+
+                        <div class="form-group">
+                            <label for="formGroupExampleInput2">Dokument (tylko .doc, .docx, .pdf):</label>
+                            <div class="custom-file">
+                                <input type="file" name="fileToUpload" class="custom-file-input" id="inputGroupFile01">
+                                <label class="custom-file-label" for="inputGroupFile01">Wybierz plik..</label>
+                                <?php
+                                if ($file==1) {
+                                    echo "Nie dodano pliku";
+                                }
+                                ?>
+                            </div>
+                        </div>
+
+
+                        <div class="form-group">
+                            <label for="formGroupExampleInput2">Identyfikator własny:</label>
+                            <input name="document_id" type="text" class="form-control" id="formGroupExampleInput2" placeholder="Wprowadź nazwę faktury..">
+                            <?php
+                            if ($id==1) {
+                                echo "Nie dodano identyfikatora";
+                            }
+                            ?>
+                        </div>
+                        <div class="form-group">
+                            <label for="formGroupExampleInput2">Notatki:</label>
+                            <input name="document_notes" type="text" class="form-control" id="formGroupExampleInput2" placeholder="Wprowadź nazwę faktury..">
+                        </div>
+
+
+                        <div class="form-group">
+                            <button type="submit" class="btn btn-primary btn-lg btn-block">Zapisz w bazie danych</button>
+                        </div>
+                    </form>
+
+
+                </div>
+            </div>
+        </div>
+    </div>
+
+</main>
